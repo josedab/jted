70a91ac212a0fd7885730526bf8dd0ac9950c1c5
Jose David Baena <josedab@gmail.com>
2015-02-26 14:55:47 +0000

    Talklist changes in order to parse json data. - In the future we may want to use a custom deserializer or use the UNWRAP_ROOT feature instead of hacking with wrapper classes

---
+++ src/main/java/com/technobitia/ted/model/WrapperTalk.java
@@ -0,0 +1,10 @@
+package com.technobitia.ted.model;
+
+public class WrapperTalk {
+    private Talk talk;
+
+    public Talk getTalk() {
+        return talk;
+    }
+
+}
--- src/main/java/com/technobitia/ted/response/TalkListResponse.java
+++ src/main/java/com/technobitia/ted/response/TalkListResponse.java
@@ -2,15 +2,23 @@ package com.technobitia.ted.response;
 
 import java.util.List;
 
+import jersey.repackaged.com.google.common.collect.Lists;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.technobitia.ted.model.Talk;
+import com.technobitia.ted.model.WrapperTalk;
 
 @JsonIgnoreProperties
 public final class TalkListResponse extends AbstractCountListResponse {
-    private List<Talk> talks;
+    
+    private List<WrapperTalk> talks;
 
     public List<Talk> getTalks() {
-        return talks;
+        List<Talk> result = Lists.newLinkedList();
+        for (WrapperTalk wtalk : talks) {
+            result.add(wtalk.getTalk());
+        }
+        return result;
     }
 
 }
